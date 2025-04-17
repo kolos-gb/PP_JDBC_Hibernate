12da6e3bfa34a4b024ebe6de815b88e8c6f5811a
shcherbina-nv <mailto:kolos.book@gmail.com>
2025-04-17 23:04:56 +0300

    Add the connection with the database

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/pp_db1";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "34547809A";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("Connection successful");
+        } catch (SQLException e) {
+            System.out.println("Connection failed");
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
